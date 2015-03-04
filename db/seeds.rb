01bcb3ef34a86fcc78e5741c76c687648ab0d19a
Brandan Lennox <brandan@bclennox.com>
2015-03-03 22:30:33 -0500

    More users in the seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,18 @@
 User.destroy_all
 
-me = User.create(
-  first_name: 'Brandan',
-  last_name: 'Lennox',
-  email: 'bclennox@gmail.com',
-  location: 'Raleigh, NC',
-  handle: 'bclennox',
-  password: 'supersecret',
-  password_confirmation: 'supersecret'
-)
+5.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    location: Faker::Address.city,
+    handle: Faker::Internet.user_name,
+    password: 'supersecret',
+    password_confirmation: 'supersecret'
+  )
+end
 
 100.times do
-  me.tweets.create(body: Faker::Hacker.say_something_smart)
+  user = User.all.sample
+  user.tweets.create(body: Faker::Hacker.say_something_smart)
 end
