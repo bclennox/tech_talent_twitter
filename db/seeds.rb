4fa3c2da9f67969c08254f13d26a224a4c4c0f30
Brandan Lennox <brandan@bclennox.com>
2015-03-03 23:04:10 -0500

    Fixing up the login page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 User.destroy_all
 
+User.create(
+  first_name: 'Brandan',
+  last_name: 'Lennox',
+  email: 'bclennox@gmail.com',
+  location: 'Raleigh',
+  handle: 'bclennox',
+  password: 'supersecret',
+  password_confirmation: 'supersecret'
+)
+
 5.times do
   User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     location: Faker::Address.city,
-    handle: Faker::Internet.user_name,
+    handle: Faker::Internet.user_name(nil, []),
     password: 'supersecret',
     password_confirmation: 'supersecret'
   )
